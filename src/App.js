17743d2e186a5e6085d1322fe6d1b95f5a6739c2
KSHITIJ KUMAR <kumarkshitij2015@gmail.com>
2022-07-03 20:26:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Covid from "./components/covid.js"
+import "./components/covid.css"
+const App =() => {
+  return (
+    <>
+    <Covid/>
+    </>
+
+  )
+}
+
+export default App;
